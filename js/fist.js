497d7c72ff8a6d3c76454757a43ddf902c75afe7
vatiza <rakibur.mac@gmail.com>
2023-02-13 01:34:30 +0600

    add summaryfile.html

---
+++ js/fist.js
@@ -0,0 +1,11 @@
+console.log("first js file");
+const allLi = document.getElementsByTagName('li');
+console.log(allLi)
+
+const allTitles = document.getElementsByClassName('section-title');
+console.log(allTitles);
+
+const secondSection = document.getElementById('second-section');
+console.log(secondSection)
+secondSection.style.color = 'white';
+secondSection.style.backgroundColor = 'tomato';
--- js/script1.js
+++ js/script1.js
@@ -1,4 +1,14 @@
-const someLi = document.querySelectorAll('.fruits-container li');
-for (const li of someLi) {
-    console.log(li.innerText);
+const sections = document.querySelectorAll('section');
+
+for (const section of sections) {
+    section.style.border = '2px solid steelblue';
+    section.style.marginBottom = '10px'
+    section.style.borderRadius = '20px';
+    section.style.paddingLeft = '8px';
+    section.style.backgroundColor='gray'
+    console.log(sections);
 }
+// const eudname = document.getElementById('edu-container');
+// eudname.style.backgroundColor='yellow'
+const placeContainer = document.getElementById('textcenter');
+placeContainer.classList.add("yello-bg");
