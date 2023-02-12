9ffa0955a813554c7e28bf485227fa1245142ba1
vatiza <rakibur.mac@gmail.com>
2023-02-13 01:59:24 +0600

    end

--- js/second.js
+++ js/second.js
@@ -1,1 +1,5 @@
 console.log('second js file');
+const secondList = document.getElementById('second-list');
+const li = document.createElement('li');
+li.innerText = "my dynaminc site";
+secondList.appendChild(li);
